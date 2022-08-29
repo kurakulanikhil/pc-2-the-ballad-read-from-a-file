09ef60dc081a4023c9faf327a7b87215b4b9ac62
nikhi <nikhildhoni17@gmail.com>
2022-08-28 20:39:23 -0500

    completed

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,13 +1,42 @@
 package com.jap.ballad;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Ballad {
+    public static void main(String[] args) {
+        Ballad obj = new Ballad();
+        String fileData = obj.readPoemFromAFile("src/main/resources/ballad.txt");
+        System.out.println(fileData);
+
+    }
     public String readPoemFromAFile(String fileName){
        // Write the logic to read from a file
-        return "";
+        FileReader fileReader = null;
+        String result = "";
+        try{
+             fileReader = new FileReader(fileName);
+            BufferedReader bufferedreader = new BufferedReader(fileReader);
+            String data = "";
+
+            while((data = bufferedreader.readLine()) != null) {
+
+
+
+
+                result = result.concat("\n") + data;
+
+            }
+
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+
+        result.replace("\n","");
+        return result;
     }
 
 
